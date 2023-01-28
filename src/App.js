ee8cd4066a806eaa48422ad21facfdf6f614f64e
iamsh2580 <iamsh2580@gmail.com>
2023-01-28 17:05:33 +0900

    state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // eslint-disable-next-line
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function Header(props){
   console.log('props',props,props.title)
@@ -18,7 +19,7 @@ function Nav(props){
     lis.push(<li key={t.id}>
       <a id={t.id} href={'/read'+t.id} onClick={event=>{
         event.preventDefault();
-        props.onChangeMode(event.target.id);
+        props.onChangeMode(Number(event.target.id));
       }}>{t.title}</a>
       </li>)
   }
@@ -36,20 +37,44 @@ function Article(props){
 
 }
 function App() {
+//  const _mode=useState('WELCOME');
+//  const mode=_mode[0]; //이 모드의 값을 통해 상태값을 읽을 수 있다.
+//  const setMode=_mode[1]; //모드의 값을 바꿀 수 있다.
+  //useState의 인자는 state의 초기값 값은0번째 바꿀때는 1번째 인덱스의 함수로
+  const[mode,setMode]=useState('WELCOME'); //과 같다. 중요!
+  const [id,setId]=useState(null);
   const topics=[
     {id:1,title:'html',body:'html is...'},
     {id:2,title:'css',body:'css is...'},
     {id:3,title:'JS',body:'JS is...'}
   ]
+  let content=null;
+  if(mode ==='WELCOME'){
+    content=<Article title="Welcome" body="Hello,WEB"></Article>
+  }else if(mode ==='READ'){
+    let title,body =null;
+
+    for(let i=0;i<topics.length;i++){
+      if(topics[i].id === id){
+        // eslint-disable-next-line
+          title=topics[i].title; 
+          // eslint-disable-next-line
+          body=topics[i].body; 
+      }
+    }
+    // eslint-disable-next-line
+    content=<Article title={title} body={body}></Article> 
+  }
   return (
     <div className="App">
-      <Header title="Web" onChangeMode={()=>{
-        alert('Header');
+      <Header title="WEB" onChangeMode={()=>{
+        setMode('WELCOME');
       }}></Header>
-      <Nav topics={topics} onChangeMode={(id)=>{
-        alert(id);
+      <Nav topics={topics} onChangeMode={(_id)=>{
+        setMode('READ');
+        setId(_id);
       }}></Nav>
-      <Article title="Welcome" body="Hello,WEB"></Article>
+      {content}
     </div> 
   );
 }
